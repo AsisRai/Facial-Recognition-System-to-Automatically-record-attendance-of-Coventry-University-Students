8ca7ed392fea60395ee248d0773c3bd5843a043f
Asis Rai <raia10@uni.coventry.ac.uk>
2018-04-02 05:55:10 +0100

    Camera Function Implemented
    Camera Function Implemented

--- FRSystem_AsisRai/FRSystem_AsisRai/Form1.Designer.cs
+++ FRSystem_AsisRai/FRSystem_AsisRai/Form1.Designer.cs
@@ -28,13 +28,71 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(663, 27);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(282, 102);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Camera";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(824, 514);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "© Asis Rai, 2018";
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(126, 27);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(493, 387);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(969, 541);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Record Attendance with Facial Recognition";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private Emgu.CV.UI.ImageBox imageBox1;
+    }
 }
 
--- FRSystem_AsisRai/FRSystem_AsisRai/Form1.cs
+++ FRSystem_AsisRai/FRSystem_AsisRai/Form1.cs
@@ -7,14 +7,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+
 
 namespace FRSystem_AsisRai
 {
 	public partial class Form1 : Form
 	{
+        Capture grabber; //to open the camera 
+        Image<Bgr, byte> currentFrame; //to capture image  
+
 		public Form1()
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) //start camera button 
+        {
+            grabber = new Capture(); //when click camera wil be opened
+            // 1.initializing the grabber event 
+            grabber.QueryFrame();
+            // 2.Now to capture the video 
+            Application.Idle += new EventHandler(FrameGrabber); //if the application is idel and the camera is on then call the frame grabber event 
+            // 3.initializing frame grabber 
+
+        }
+
+        void FrameGrabber(object sender, EventArgs e) //Frame grabber event 
+        {
+            //initialize current frame with query grabber which is catching the frame
+            currentFrame = grabber.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC); //resizing the frame with cubic frame
+            //Now to view current frame in the imported ImageBox
+            imageBox1.Image = currentFrame; //current frame = captured from the camera into the imagebox
+            //initialize the currentframe
+        }
+    }
 }
